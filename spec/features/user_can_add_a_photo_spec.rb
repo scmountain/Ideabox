fd7b263c52c658d2befd676710edba4e028d48ec
Spencer Carter <scmountain@gmail.com>
2017-02-21 01:22:27 -0700

    testing of photos index, plus navigational links added

--- spec/features/user_can_add_a_photo_spec.rb
+++ spec/features/user_can_add_a_photo_spec.rb
@@ -25,5 +25,14 @@ RSpec.feature "user can add photos" do
                       password: 'pass',
                       password_confirmation: "pass")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    Photo.create(url: "http://kingofwallpapers.com/gnome/gnome-010.jpg",
+                caption: "string-kini", category: "funny")
+    Photo.create(url: "http://www.gardenandpatiohomeguide.com/wp-content/uploads/2016/04/81u5eFmoiWL._SL1500_-1024x1024.jpg",
+                caption: "zen", category: "travel")
+
+    visit photos_path
+    expect(page).to have_content("string-kini")
+    expect(page).to have_content("zen")
   end
 end
